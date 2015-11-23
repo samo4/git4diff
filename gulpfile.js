372f7a67368b39a2482c06ed3e2a770c5e32240d
Samo Fabcic <samo.fabcic@gmail.com>
2015-11-23 13:06:43 +0100

    new content and improvoment to gulp usage

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,24 @@ var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 
 gulp.task('dojs', function() {
-	return gulp.src('./node_modules/reveal.js/js/*.js', {base: './node_modules/reveal.js'})
+	gulp.src(['./node_modules/reveal.js/js/*.js', './node_modules/reveal.js/lib/js/head.min.js'], {base: './node_modules/reveal.js'})
 		.pipe(concat('js/scripts.js'))
+		//.pipe(uglify())
+		.pipe(gulp.dest('./public/'));
+		
+	gulp.src(['./node_modules/reveal.js/plugin/markdown/*.js', './node_modules/reveal.js/plugin/highlight/*.js'], {base: './node_modules/reveal.js'})
 		//.pipe(uglify())
 		.pipe(gulp.dest('./public/'));
 });		
 
 gulp.task('docss', function() {
-	return gulp.src('./node_modules/reveal.js/css/**/*.css', {base: './node_modules/reveal.js'})
+	 gulp.src('./src/css/*.css')
+		.pipe(minifyCss({
+		  keepSpecialComments: 0
+		}))
+		.pipe(rename({ extname: '.min.css' }))
+		.pipe(gulp.dest('./public/css'));
+	gulp.src(['./node_modules/reveal.js/css/**/*.css', './node_modules/reveal.js/lib/css/*.css'], {base: './node_modules/reveal.js'})
 		//.pipe(sourcemaps.init())
 		.pipe(minifyCss({
 		  keepSpecialComments: 0
